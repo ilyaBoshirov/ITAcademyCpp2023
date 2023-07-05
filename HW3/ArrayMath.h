42d8f11f91e77f97e0c5884119512b5a94121fc7
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-07-05 19:51:51 +0300

    add functions definitions

---
+++ HW3/ArrayMath.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+namespace ArrayMath {
+	void minEvenElement(int* arr, size_t arraySize);
+
+	uint32_t getMinAndMaxSumNuber(int* arr, size_t arraySize);
+
+	int getOddElementsProduct(int* arr, size_t arraySize);
+
+	void simpleSwap(int* arr, int i, int j);
+
+	void smartSwap(int* arr, int i, int j);
+}
---
+++ HW3/VectorMath.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+namespace VectorMath {
+	void minEvenElement(std::vector<int> vec);
+
+	uint32_t getMinAndMaxSumNuber(std::vector<int> vec);
+
+	int getOddElementsProduct(std::vector<int> vec);
+
+	void simpleSwap(std::vector<int>& vec, int i, int j);
+
+	void smartSwap(std::vector<int>& vec, int i, int j);
+
+	void stdSwap(std::vector<int>& vec, int i, int j);
+}
