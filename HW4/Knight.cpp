21769a7d7daa4624c6be0ca8bd7fa56d6bf06ec8
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-14 18:08:32 +0300

    Create class Knight

--- HW4/Knight.cpp
+++ HW4/Knight.cpp
@@ -3,14 +3,40 @@
 
 Knight::Knight() : Creature(), sword() {
 	this->name = "Arthur";
+	this->killedEnemyCounter = 0;
 }
 
-Knight::Knight(std::string name) : Creature(), sword(), name(name) {};
+Knight::Knight(std::string name) : Creature(), sword(), name(name), killedEnemyCounter{0} {};
 
-Knight::Knight(int health, int strength, std::string name) : Creature(health, strength), sword(), name(name) {};
+Knight::Knight(int health, int strength, std::string name) : Creature(health, strength), sword(), name(name), killedEnemyCounter{ 0 } {};
 
-Knight::Knight(int health, int strength, std::string name, Sword sword) : Creature(health, strength), sword(sword), name(name) {};
+Knight::Knight(int health, int strength, std::string name, Sword sword) : Creature(health, strength), sword(sword), name(name), killedEnemyCounter{ 0 } {};
 
 int Knight::makeDamage() {
 	return this->getStrength() + this->sword.getDamage();
+}
+
+void Knight::addVictory() {
+	this->killedEnemyCounter += 1;
+	if (killedEnemyCounter % 3 == 0) {
+		this->sword.update();
+	}
+}
+
+void Knight::healing() {
+	this->currentHealth = this->initHealth;
+}
+
+Knight& Knight::operator= (const Knight& knight) {
+	if (this == &knight) {
+		return *this;
+	}
+
+	this->initHealth = knight.initHealth;
+	this->currentHealth = knight.currentHealth;
+	this->strength = knight.strength;
+	this->sword = knight.sword;
+	this->name = knight.name;
+
+	return *this;
 }
--- HW4/Knight.h
+++ HW4/Knight.h
@@ -8,6 +8,7 @@
 class Knight : public Creature {
 	std::string name;
 	Sword sword;
+	uint32_t killedEnemyCounter;
 
 public:
 
@@ -15,6 +16,11 @@ public:
 	Knight(std::string name);
 	Knight(int health, int strength, std::string name);
 	Knight(int health, int strength, std::string name, Sword sword);
+
+	void addVictory();
+	void healing();
+	
+	Knight& operator= (const Knight& knight);
 
 	virtual int makeDamage();
 };
