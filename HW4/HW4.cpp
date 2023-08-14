46650f0eae38d9b2d59016866e26c6f32104af11
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-14 18:09:06 +0300

    Update HW4.cpp

--- HW4/HW4.cpp
+++ HW4/HW4.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <string>
+
+#include "Knight.h"
+#include "Orc.h"
+#include "Sword.h"
+
+
+Knight createKnightHero() {
+	int health = 200;
+	int strength = 10;
+	std::string name = "Arthur";
+	Sword heroSword{ 20 };
+
+	return Knight{ health, strength, name, heroSword };
+}
+
+Orc createBossOrc() {
+	int health = 150;
+	int strength = 50;
+	std::string name = "Ekganit";
+
+	return Orc{ health, strength, name };
+}
+
+Orc createOrdinaryOrc() {
+	return Orc{};
+}
+
+void menu() {
+	Knight hero = createKnightHero();
+	Orc boss = createBossOrc();
+
+}
 
 int main() {
-	std::cout << "OK" << std::endl;
+	menu();
+	return 0;
 }
