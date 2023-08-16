252ffb6a3a7c07fffb1027ad91a530f6157f4c37
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-16 11:37:01 +0300

    Update HW4.cpp

--- HW4/HW4.cpp
+++ HW4/HW4.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 #include "Knight.h"
 #include "Orc.h"
 #include "Sword.h"
+
+constexpr auto SLEEP_TIME = 1000;
 
 
 Knight createKnightHero() {
@@ -35,6 +38,7 @@ void printIntroduction(std::string heroName, std::string bossName) {
 }
 
 void printGameMenu() {
+	std::cout << std::endl;
 	std::cout << "1 - print hero status;" << std::endl;
 	std::cout << "2 - kill some orc;" << std::endl;
 	std::cout << "3 - cure the hero;" << std::endl;
@@ -42,14 +46,60 @@ void printGameMenu() {
 	std::cout << "0 - exit game." << std::endl;
 }
 
+int fightWithOrc(Knight& knight, Orc& orc, bool isBoss) {
+	/*
+	Function return:
+	0 - if you win
+	1 - if you lose
+	*/
+
+	std::string enemyType = isBoss ? "orc leader " + orc.getName() : "orc";
+
+	std::cout << "Oh no, you met " << enemyType << " with " << orc.getHealth() << " HP and " << orc.getStrength() << " strength" << std::endl;
+	std::cout << "Let's fight!" << std::endl;
+
+	int damage;
+	while (knight.getHealth() > 0 || orc.getHealth() > 0) {
+		Sleep(SLEEP_TIME);
+		damage = knight.makeDamage();
+		orc.getDamage(damage);
+		std::cout << "You dealt " << damage << " damage to the orc" << std::endl;
+
+		if (orc.getHealth() == 0) {
+			break;
+		}
+
+		Sleep(SLEEP_TIME);
+		damage = orc.makeDamage();
+		knight.getDamage(damage);
+		std::cout << "You took " << damage << " damage from the orc" << std::endl;
+		
+		std::cout << "Your HP: " << knight.getHealth() << "; Enemy HP: " << orc.getHealth() << "." << std::endl;
+		
+		std::cout << std::endl;
+	}
+
+	if (knight.getHealth() == 0) {
+		std::cout << "Oh no, you lose!" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Congratulations you killed the " << enemyType << "!" << std::endl;
+	knight.addVictory();
+	return 0;
+
+}
+
 void playGame(Knight knight, Orc boss) {
 	printIntroduction(knight.getName(), boss.getName());
 	system("pause");
 
-	bool gameStatus = true;
+	bool gameEnd = false;
 	int userChoice;
-	while (gameStatus) {
-		system("cls");
+	int fightResult;
+	Orc ordinaryOrc;
+
+	while (!gameEnd) {
 		printGameMenu();
 
 		std::cout << "Enter: ";
@@ -60,15 +110,36 @@ void playGame(Knight knight, Orc boss) {
 		{
 		case 1:
 			std::cout << knight.getStatus() << std::endl;
-			system("pause");
 			break;
 		case 2:
+			system("cls");
+			ordinaryOrc = createOrdinaryOrc();
+			fightResult = fightWithOrc(knight, ordinaryOrc, false);
+			if (fightResult) {
+				std::cout << "You died without saving the princess. Try another time. Bye!" << std::endl;
+				exit(0);
+			}
+			system("pause");
+			system("cls");
 			break;
 		case 3:
+			std::cout << "Healing hero..." << std::endl;
+			knight.healing();
 			break;
 		case 4:
+			system("cls");
+			fightResult = fightWithOrc(knight, boss, true);
+			if (fightResult) {
+				std::cout << "Boss kill you. You so weak. Try another time. Bye!" << std::endl;
+				exit(0);
+			}
+			gameEnd = true;
+			system("pause");
+			system("cls");
 			break;
 		case 0:
+			std::cout << "You didn't want to save the princess. Goodbye..." << std::endl;
+			gameEnd = true;
 			break;
 		default:
 			break;
@@ -87,6 +158,9 @@ int main() {
 	startGame();
 	return 0;
 }
+
+
+
 
 
 
