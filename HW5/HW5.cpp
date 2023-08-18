e0409279123d00eba4bb89c1ae6793799e2949f7
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-18 17:27:46 +0300

    Update HW5.cpp

--- HW5/HW5.cpp
+++ HW5/HW5.cpp
@@ -1,9 +1,21 @@
 #include <algorithm>
 #include <string>
 #include <iostream>
+#include <thread>
+
+#include <Windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include "Game.h"
 
 
 int main(int argc, char* argv[]) {
+    Game game{30, 15};
+
+    game.startGame();
+    std::thread thread(&Game::keypressListener, &game);
+
+    thread.join();
     
 	return 0;
 }
