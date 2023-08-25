c7d12a06245f4c642effa3a3ceeff1aae96dfa28
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-25 15:13:39 +0300

    Update HW6.cpp

--- HW6/HW6.cpp
+++ HW6/HW6.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <numeric>
 
 
 #include "FindPattern.h"
+#include "VectorAnalyzer.h"
 
 constexpr auto task1VectorDataFileName = "task1_vector_data.txt";
 
@@ -27,22 +29,63 @@ void printVector(const std::vector<std::string>& lines) {
 }
 
 void task1() {
+	std::cout << "*********** TASK 1 ***********" << std::endl;
+	std::cout << "Load lines from file " << task1VectorDataFileName << std::endl;
+	
 	std::vector<std::string> lines;
 	fillVector(lines);
 	printVector(lines);
 
 	std::cout << std::endl << "Enter pattern you want to search: ";
-	
 	std::string pattern;
 	std::cin >> pattern;
 
 	std::cout << "NUmber of elements contains pattern [" << pattern << "] : " << getNumberOfMatches(lines, pattern) << std::endl;
 }
 
-int main() {
-	// task 1 
-	task1();
+void task2() {
+	std::cout << "*********** TASK 2 ***********" << std::endl;
+	std::vector<int> numbers(10);
+	std::iota(numbers.begin(), numbers.end(), 1);
+
+	VectorAnalyzer<int> analyzer{ numbers };
+	analyzer.showData();
+
+	std::cout << "Square vectoe elements with std::transform" << std::endl;
+	analyzer.square();
+	analyzer.showData();
+
+	std::cout << "Enter value to add to all elements of vector: ";
 	
+	int value{ 0 };
+	std::cin >> value;
+
+	std::cout << std::endl << "Add " << value << " to elements elements with std::transform" << std::endl;
+	analyzer.plus(value);
+	analyzer.showData();
+
+	std::cout << "Test adjacent_find" << std::endl;
+	analyzer.findIdenticalNeighbors();
+
+	std::cout << "Push back 4 to vector" << std::endl;
+	analyzer.pushBack(4);
+	std::cout << "Push back 4 to vector" << std::endl;
+	analyzer.pushBack(4);
+
+	std::cout << "New vector" << std::endl;
+	analyzer.showData();
+
+	analyzer.findIdenticalNeighbors();
+}
+
+void task3() {
+	std::cout << "*********** TASK 3 ***********" << std::endl;
+}
+
+int main() {
+	// task1();
+	// task2();
+	task3();
 
 	return 0;
 }
