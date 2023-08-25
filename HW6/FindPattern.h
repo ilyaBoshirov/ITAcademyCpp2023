9e6d5bcce5cc5585e5e84f916df4ad7d1aad3444
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-25 12:10:28 +0300

    lambda for task 1

---
+++ HW6/FindPattern.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+
+
+struct Contains : public std::binary_function<std::string, std::string, bool>
+{
+	bool operator()(std::string const& item, std::string const& pattern) const
+	{
+		std::string str{ item };
+		std::transform(str.begin(), str.end(), str.begin(),
+			[](unsigned char c) { return std::tolower(c); });
+		return str.find(pattern) != std::string::npos;
+	}
+};
+
+
+
+auto getNumberOfMatches{ [](std::vector<std::string> lines, std::string pattern) {
+	return std::count_if(lines.begin(), lines.end(), std::bind2nd(Contains(), pattern));
+	}
+};
