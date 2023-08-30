03ec95e86862284ada82fa71c879777f3866fee9
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-30 15:43:50 +0300

    create class PrimeTests
    contains primality tests

---
+++ HW7/PrimeTests.cpp
@@ -0,0 +1,130 @@
+﻿// PrimeTests.cpp : Определяет экспортируемые функции для DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "PrimeTests.h"
+
+
+// Пример экспортированной переменной
+PRIMETESTS_API int nPrimeTests=0;
+
+// Пример экспортированной функции.
+PRIMETESTS_API int fnPrimeTests(void)
+{
+    return 0;
+}
+
+// Конструктор для экспортированного класса.
+CPrimeTests::CPrimeTests()
+{
+    return;
+}
+
+bool CPrimeTests::divisorIterationTest(uint64_t number)
+{
+	uint64_t root = sqrt(number);
+	for (uint64_t i = 2; i <= root; i++) {
+
+		if ((uint64_t)(number % root) == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool CPrimeTests::pFarmTest(uint64_t number, uint64_t accuracy) {
+	std::uniform_int_distribution<> die( 2, number );
+
+	std::vector<uint64_t> values{};
+	values.resize(10);
+	uint64_t temp{};
+
+	while (values.size() < accuracy) {
+		temp = die(this->mersenne);
+
+		if (this->gcd(number, accuracy) != 1) {
+			continue;
+		}
+		values.push_back(temp);
+	}
+
+	for (auto i = 0; i < accuracy; ++i) {
+		if (this->fastModulePow(values[i], number - 1, number) != 1) {
+			return false;
+		}
+	}
+
+	std::cout << "Numer " << number << " is prime with probability " << 1 - (1 / pow(2, accuracy)) << std::endl;
+
+	return true;
+}
+
+bool CPrimeTests::pMillerRabinTest(uint64_t number, uint64_t accuracy) {
+	if (number == 2 || number == 3) {
+		return true;
+	}
+
+	if (number == 1 || number % 2 == 0) {
+		return false;
+	}
+
+	uint64_t t{ number - 1 };
+	uint64_t s{ 0 };
+
+	while (t % 2 == 0) {
+		t /= 2;
+		s += 1;
+	}
+
+	std::uniform_int_distribution<> die(2, number - 2);
+
+	for (auto i = 0; i < accuracy; ++i) {
+		uint64_t a = die(this->mersenne);
+
+		uint64_t x = this->fastModulePow(a, t, number);
+
+		if (x == 1 || x == number - 1) {
+			continue;
+		}
+
+		for (auto j = 0; j < s - 1; ++j) {
+			x = this->fastModulePow(x, 2, number);
+
+			if (x == 1) {
+				return false;
+			}
+
+			if (x == number - 1) {
+				break;
+			}
+		}
+
+		if (x != number - 1) {
+			return false;
+		}
+	}
+
+	std::cout << "Numer " << number << " is prime with probability " << 1 - pow(4, -1 * accuracy) << std::endl;
+
+	return true;
+}
+
+uint64_t CPrimeTests::gcd(uint64_t a, uint64_t b) {
+	return b ? this->gcd(b, a % b) : a;
+}
+
+uint64_t CPrimeTests::fastModulePow(uint64_t base, uint64_t degree, uint64_t mod) {
+	uint64_t result{ 1 };
+	uint64_t temp{ base };
+
+	for (uint64_t i = degree; i > 0; i >>= 1) {
+		if (i & 0x1 == 1) {
+			result = (result * temp) % mod;
+		}
+		temp *= temp;
+		temp %= mod;
+	}
+
+	return result;
+}
---
+++ HW7/PrimeTests.h
@@ -0,0 +1,37 @@
+﻿// Приведенный ниже блок ifdef — это стандартный метод создания макросов, упрощающий процедуру
+// экспорта из библиотек DLL. Все файлы данной DLL скомпилированы с использованием символа PRIMETESTS_EXPORTS
+// Символ, определенный в командной строке. Этот символ не должен быть определен в каком-либо проекте,
+// использующем данную DLL. Благодаря этому любой другой проект, исходные файлы которого включают данный файл, видит
+// функции PRIMETESTS_API как импортированные из DLL, тогда как данная DLL видит символы,
+// определяемые данным макросом, как экспортированные.
+#ifdef PRIMETESTS_EXPORTS
+#define PRIMETESTS_API __declspec(dllexport)
+#else
+#define PRIMETESTS_API __declspec(dllimport)
+#endif
+
+#include <iostream>
+#include <random>
+
+// Этот класс экспортирован из библиотеки DLL
+class PRIMETESTS_API CPrimeTests {
+
+	std::mt19937_64 mersenne{ static_cast<std::mt19937_64::result_type>(std::time(nullptr)) };
+
+public:
+	CPrimeTests(void);
+	
+	bool divisorIterationTest(uint64_t number);
+
+	bool pFarmTest(uint64_t number, uint64_t accuracy);
+
+	bool pMillerRabinTest(uint64_t number, uint64_t accuracy);
+
+	uint64_t gcd(uint64_t a, uint64_t b);
+
+	uint64_t fastModulePow(uint64_t base, uint64_t degree, uint64_t mod);
+};
+
+extern PRIMETESTS_API int nPrimeTests;
+
+PRIMETESTS_API int fnPrimeTests(void);
