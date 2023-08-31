b37d413f0da6304b2d1d497080eeab6b4b98e281
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-31 15:07:38 +0300

    create class LFSR
    Linear feedback shift register

---
+++ HW8/LFSR.cpp
@@ -0,0 +1,46 @@
+#include "LFSR.h"
+
+LFSR::LFSR() {
+	this->initState = 0x1;
+	this->state = this->initState;
+}
+
+LFSR::LFSR(uint16_t state) {
+	this->initState = state;
+	this->state = this->initState;
+}
+
+uint16_t LFSR::getState() {
+	return this->state;
+}
+
+void LFSR::setState(uint16_t state) {
+	this->state = state;
+}
+
+uint16_t LFSR::getBit() {
+	uint16_t x1 = (this->state >> (this->lfsrLength - 1)) & 0x1;
+	uint16_t x2 = (this->state >> (this->lfsrLength - 5)) & 0x1;
+	uint16_t x3 = this->state & 0x1;
+
+	return (x1 & x2) ^ (x1 & x3) ^ (x2 & x3);
+}
+
+void LFSR::init() {
+	for (auto i{ 0 }; i < INITROUND; ++i) {
+		this->rotate();
+	}
+}
+
+void LFSR::rotate() {
+	uint16_t x16 = (this->state >> (this->lfsrLength - 1)) & 0x1;
+	uint16_t x15 = (this->state >> (this->lfsrLength - 2)) & 0x1;
+	uint16_t x13 = (this->state >> (this->lfsrLength - 4)) & 0x1;
+	uint16_t x4 = (this->state >> (this->lfsrLength - 13)) & 0x1;
+
+	this->state = ((this->state << 1) & 0xfffe) | (x16 ^ x15 ^ x13 ^ x4 );
+}
+
+void LFSR::reset() {
+	this->state = this->initState;
+}
---
+++ HW8/LFSR.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+constexpr auto INITROUND = 30;
+
+class LFSR
+{
+	uint16_t initState;
+	uint16_t state;
+
+	size_t lfsrLength{ 16 };
+
+public:
+	LFSR();
+	LFSR(uint16_t state);
+
+	uint16_t getState();
+	void setState(uint16_t state);
+
+	uint16_t getBit();
+	void init();
+	void rotate();
+	void reset();
+};
