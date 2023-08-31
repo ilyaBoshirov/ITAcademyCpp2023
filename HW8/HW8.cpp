80607948e4f9093a1bf4c69a4958525b136f0c21
ilyaBoshirov <43990343+ilyaBoshirov@users.noreply.github.com>
2023-08-31 17:52:48 +0300

    Update HW8.cpp

--- HW8/HW8.cpp
+++ HW8/HW8.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <atomic>
 
 #include "LFSR.h"
+
+struct Answer {
+	uint16_t candidate{ 0x1 };
+	std::vector<uint16_t> solution{};
+};
+
+void bruteFunction() {
+	LFSR lfsr{};
+	auto state = 0x1;
+
+	while (state != 0) {
+
+
+
+		state = 0x1;
+	}
+}
 
 
 int main() {
